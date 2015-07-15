1d164db1b728851e2aea77f3be2a09d37d01477b
kalyandechiraju <kalyandechiraju@outlook.com>
2015-07-15 20:28:23 +0530

    Moving to github

--- app/src/main/java/com/codelight/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/codelight/sunshine/MainActivityFragment.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -12,12 +18,34 @@ import android.view.ViewGroup;
  */
 public class MainActivityFragment extends Fragment {
 
+    private ArrayAdapter<String> mForecastAdapter;
+
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        String[] forecastList = {
+                "Today - Sunny - 37/21",
+                "Today - Sunny - 37/21",
+                "Today - Sunny - 37/21",
+                "Today - Sunny - 37/21",
+                "Today - Sunny - 37/21",
+                "Today - Sunny - 37/21",
+                "Today - Sunny - 37/21"
+        };
+
+        List<String> weekForecast = new ArrayList<>(Arrays.asList(forecastList));
+
+        mForecastAdapter = new ArrayAdapter<>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mForecastAdapter);
+
+        return rootView;
     }
 }
